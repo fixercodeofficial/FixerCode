029b6ea1fe65fdd4af04466bc8b91c698783c1fe
Arka-AchieveX <Arka-AchieveX@DESKTOP-PC7NV1F>
2024-05-13 14:46:05 +0530

    Add project files.

---
+++ FixerCode/Areas/Customer/Controllers/FrontendController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FixerCode.Areas.Customer.Controllers;
+
+[Area("Customer")]
+public class FrontendController : Controller
+{
+    private readonly ILogger<FrontendController> _logger;
+
+    public FrontendController(ILogger<FrontendController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+
+
+}
